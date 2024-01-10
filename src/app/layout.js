b13856b7dac8b77cc180ce7a193611b2ad71c00a
Pedro Henrique Almeida <pedro.almeidah@ufu.br>
2024-01-10 16:52:52 -0300

    mudanças no layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,15 +3,44 @@
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { CacheProvider } from '@chakra-ui/next-js'
+import { Grid, GridItem } from '@chakra-ui/react'
+import { Header } from '/c/users/dev/Desktop/issoeabusosim/src/components/Header.jsx'
+import Sidebar from '/c/users/dev/Desktop/issoeabusosim/src/components/Sidebar.jsx'
 
 const titles = Nunito({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <ChakraProvider>
-        <body className={titles.className}>{children}</body>
-      </ChakraProvider>
+      <CacheProvider>
+        <ChakraProvider>
+          <body className={titles.className}>
+            <Grid
+              templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+              gridTemplateRows={'1fr'}
+              gridTemplateColumns={' 1fr'}
+              h="200px"
+              gap="3"
+              color="blackAlpha.700"
+              fontWeight="bold"
+            >
+              <GridItem pl="2" area={'header'}>
+                <Header />
+              </GridItem>
+              <GridItem pl="2" area={'nav'}>
+                <Sidebar />
+              </GridItem>
+              <GridItem pl="2" area={'main'}>
+                {children}
+              </GridItem>
+              <GridItem pl="2" area={'footer'}></GridItem>
+            </Grid>
+          </body>
+        </ChakraProvider>
+      </CacheProvider>
     </html>
   )
 }
